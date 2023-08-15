e7b008b9869f3d00c5513fec1585226537afa585
Hajardiyaa <hajardiyae12@gmail.com>
2023-08-15 08:45:41 -0700

    updated.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """Find the biggest int"""
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for n in range(len(my_list)):
+        if my_list[n] > big:
+            big = my_list[n]
+
+    return (big)
+
+
