e9d5ca31ffe3957e98e0061371e2f779b5aaebfc
Hajardiyaa <hajardiyae12@gmail.com>
2023-09-07 04:29:05 -0700

    classes.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Containing the rectangle class"""
+
+
+class Rectangle:
+    """A rectangle class"""
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Containing the rectangle class"""
+
+
+class Rectangle:
+    """A rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Rectangle initialization"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Width property function"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """Width set"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """Height property function"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Height set"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
