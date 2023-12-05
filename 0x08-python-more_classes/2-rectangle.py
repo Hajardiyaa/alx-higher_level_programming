0aaffc7fdfbcd80748504dd2f38bb233ed0c56fb
Hajardiyaa <hajardiyae12@gmail.com>
2023-12-05 08:11:33 -0800

    2nd commit-v2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,40 +1,49 @@
 #!/usr/bin/python3
-"""class Rectangle"""
+"""the rectangle class"""
+
+
 class Rectangle:
+    """rectangle class"""
+
     def __init__(self, width=0, height=0):
-"""inizializing the rectangle"""
-        self.__width = width
-        self.__height = height
+        """Initilizes a rectangle"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-"""width proprety"""
+        """width property"""
         return self.__width
 
     @width.setter
-    def width(self, value):
-"""width setter"""
-        if not isinstance(value, int):
+    def width(self, width):
+        """width settings"""
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
+        """height property"""
         return self.__height
 
     @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
+    def height(self, height):
+        """height settings"""
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = height
 
     def area(self):
-        return self.__width * self.__height
+        """Returning the area of the rectangle"""
+        return self.height * self.width
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
-
+        """Returning the perimeter of a rectangle"""
+        if self.height == 0 or self.width == 0:
+            return 0
+        return self.height * 2 + self.width * 2
