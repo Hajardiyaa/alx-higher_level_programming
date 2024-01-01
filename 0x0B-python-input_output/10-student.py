334eed4bf77248375781f2112bd45ebe5e15a64d
Hajardiyaa <hajardiyae12@gmail.com>
2024-01-01 10:53:39 +0000

    5

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a class Student that defines a student by: (based on 9-student.py)"""
+
+
+class Student:
+    """Represent student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing the new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a filtered dictionary """
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    filtered_dict[attr] = self.__dict__[attr]
+            return filtered_dict
